61310295bf89a29abe61b217a72b3a58d3f56c6e
__yuya <yuya1999.1130@me.com>
2020-10-10 23:21:37 +0900

    second commit

---
+++ app/Http/Controllers/Chuhai_Review_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Chuhai_Review_Controller extends Controller
+{
+    //
+}
---
+++ database/factories/ChuhaiReviewFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Chuhai_Review;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Chuhai_ReviewFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Chuhai_Review::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
